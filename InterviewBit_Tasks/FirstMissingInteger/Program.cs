6bc853192ac65afce4e9d6712029d93d9e5c4e50
asyafl <65190854+asyafl@users.noreply.github.com>
2021-02-03 15:09:58 +0200

    First Missing Integer
    https://www.interviewbit.com/problems/first-missing-integer/

---
+++ InterviewBit_Tasks/FirstMissingInteger/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FirstMissingInteger
+{
+
+    /// <summary>
+    /// Given an unsorted integer array, find the first missing positive integer.
+    /// Example:
+    /// Given [1,2,0] return 3,
+    /// [3,4,-1,1] return 2,
+    /// [-8, -7, -6] returns 1
+    /// Your algorithm should run in O(n) time and use constant space.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            var arr = new int[] { 3, 4, -1, 1};
+
+            List<int> A = arr.ToList();
+
+            Console.WriteLine(s.FirstMissingPositive(A));
+        }
+
+    }
+
+    public class Solution
+    {
+        public int FirstMissingPositive(List<int> A)
+        {
+            var posSet = new HashSet<int>();
+
+            for (int i = 0; i < A.Count; i++)
+            {
+                if (A[i] > 0)
+                {
+                    posSet.Add(A[i]);
+                }
+            }
+
+            var count = posSet.Count;
+
+            if(count == 0)
+            {
+                return 1;
+            }
+
+            var missingInt = count + 1; 
+
+            for(int i = 1; i <= count; i++)
+            {
+                if (!posSet.Contains(i))
+                {
+                    return i;
+                }
+            }
+            return missingInt;
+        }
+    }
+}
