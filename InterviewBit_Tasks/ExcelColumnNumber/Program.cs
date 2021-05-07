5d14e35638dd2643b1b6abacfa26e4441bc8def7
asyafl <65190854+asyafl@users.noreply.github.com>
2021-05-07 15:49:18 +0300

    imp Reverse int

---
+++ InterviewBit_Tasks/ExcelColumnNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExcelColumnNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            Console.WriteLine(s.titleToNumber("AB"));
+        }
+    }
+
+    public class Solution
+    {
+        public int titleToNumber(string A)
+        {
+            int res = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                res *= 26;
+                res += (A[i] - 'A' + 1);
+            }
+
+            return res;
+        }
+    }
+}
---
+++ InterviewBit_Tasks/ReverseInteger/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ReverseInteger
+{
+    /// <summary>
+    /// Reverse digits of an integer.
+    /// Example1: x = 123,  return 321
+    /// Example2: x = -123, return -321
+    /// Return 0 if the result overflows and does not fit in a 32 bit signed integer
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            Console.WriteLine(s.Reverse(-1146467285));
+        }
+    }
+    public class Solution
+    {
+        public int Reverse(int A)
+        {
+            string str = A.ToString();
+            var res = "";
+
+            if (A < 0)
+            {
+                res = "-";
+                for (int i = str.Length - 1; i > 0; i--)
+                {
+                    res += str[i];
+                }
+            }
+            else
+            {
+                for (int i = str.Length - 1; i >= 0; i--)
+                {
+                    res += str[i];
+                }
+            }
+
+            if (int.TryParse(res, out var returnVal))
+            {
+                return returnVal;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+    }
+}
