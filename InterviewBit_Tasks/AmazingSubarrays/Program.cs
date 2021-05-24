d04db38a1e38b8291adaaebc1b25c7e8534660c8
asyafl <65190854+asyafl@users.noreply.github.com>
2021-05-24 21:04:15 +0300

    solve task about subarrays

---
+++ InterviewBit_Tasks/AmazingSubarrays/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AmazingSubarrays
+{
+    /// <summary>
+    /// You are given a string S, and you have to find all the amazing substrings of S.
+    /// Amazing Substring is one that starts with a vowel(a, e, i, o, u, A, E, I, O, U).
+    /// Return a single integer X mod 10003, here X is number of Amazing Substrings in given string.
+    ///Constraints: 1 <= length(S) <= 1e6 ; S can have special characters
+    ///Input: ABEC , Output: 6
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            Console.WriteLine(s.Solve("AzZGBauYuTknYjjWEEbLvqMQwnoSgXKBdHKEVpeoxYNNtBfrxQrPauttMzjKFayKwMeuChjzCocgAHfAmPCatOqarzLuabyTnxoheeocVshfERNssVPfRyPmwvOVGbzNAuvryYNWwIeyZLMlLbkcFFJRHjEIgIwOThRJJFpLbunVrbhAYsMtdsKslLAGElwrZjvZweIytMpPEYVmktQeNleNROcTjrNxXeHvOMMTMfqZHTUjetojHFzZwOekfAILYISANxeJFRNgeZDKoOTddXqxduPIjGXsRSSkgIqKMeSqlQwAKtdrkvHLgmKleNAPEztGMPmZzUuCImLAhzUnXmsVzFvJUTeIKleuRnMPNAPWJYAZLXgzTBPmkXVShbBSlIAJSeFgvRFvcoqfVFgHUefxUxuYFCfUxbxlOslUhYRFXKmMmqDIQhhfXyGqUwGMSYeLLpEsKAhvFUzavDCOUgtmmNMnsvfmCdPWuWIjuUfZCBTrWnaDopbqXcjzSqRMpQWIBNnMcOQZjDkjPkxsuwENYQyjgSHFJrgSLnwbEInBfdeIfBbVuZZbBrblJgKHOmLZACLQkSRxxQJeUMPIQutraxFtrRcSeqAejOTSqaFGglQOoWPkcNOnLIgfclWNtjGQRVMlqCPUnUlOLbHfkzUyNAmTsswWtZjIGUBrLmRmstHgVcRUgWdQTCEPRzjPVTMJRJocYHftwoRzOSyQexjmceRHdqFdgNuGmGTUdXQaNwKmvOUzZPzGCBVcbVLgMoQrESbpVGteVVntOwEWxXsZnSAoIfBSsWVhDFBuDTkcrnsPdmmSHymouxHlcgtjgKUAPznxsIRUjDFsrjadJjEtPaWTVBHpatqYeSgrpWJDOGfgIGQPcTIXVsCVyCfKMpcXWGkvwuRuTmvCbNMLeUkZrEpYZdlKAgFELfwCCbZCYBcXhfUrsIHfdwhYyxHKAMYMERwlyRtxObDoxBhjXmynYkmkYZrkzlCuvrhW"));
+        }
+    }
+    public class Solution
+    {
+        public int Solve(String A)
+        {
+            var set = new HashSet<char>() { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+            var result = 0;
+
+            var tempVal = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (set.Contains(A[i]))
+                {
+                    tempVal++;
+                }
+
+                if (tempVal > 0)
+                {
+                    result += tempVal;
+                }
+            }
+
+            return result % 10003;
+
+        }
+    }
+}
---
+++ InterviewBit_Tasks/CountAndSay/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CountAndSay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var s = new Solution();
+            Console.WriteLine(s.CountAndSay(2));
+        }
+    }
+    public class Solution
+    {
+        public string CountAndSay(int A)
+        {
+            if(A == 1)
+            {
+                return "1";
+            }
+            if(A == 2)
+            {
+                return "11";
+            }
+
+            var str = "11";
+            for(int i = 3; i<= A; i++)
+            {
+                str += '$';
+                int len = str.Length;
+
+                int cnt = 1; 
+                string tmp = ""; 
+                char[] arr = str.ToCharArray();
+
+                
+                for (int j = 1; j < len; j++)
+                {
+                    
+                    if (arr[j] != arr[j - 1])
+                    {
+                        
+                        tmp += cnt + 0;
+
+                        tmp += arr[j - 1];
+
+                        cnt = 1;
+                    }
+
+                    else cnt++;
+                }
+                str = tmp;
+            }
+
+            return str;
+        
+        }
+    }
+}
