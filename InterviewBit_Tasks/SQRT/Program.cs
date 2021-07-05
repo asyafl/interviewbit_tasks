14dedbffe378703a6e5eea50367d683df91084b8
asyafl <65190854+asyafl@users.noreply.github.com>
2021-07-05 17:26:55 +0300

    compute sqrt

---
+++ InterviewBit_Tasks/SQRT/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SQRT
+{
+    /// <summary>
+    /// Given an integar A.
+    /// Compute and return the square root of A.
+    /// If A is not a perfect square, return floor(sqrt(A)).
+    /// DO NOT USE SQRT FUNCTION FROM STANDARD LIBRARY
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            Console.WriteLine(s.Sqrt(2147483647));
+        }
+    }
+
+    public class Solution
+    {
+        public int Sqrt(int A)
+        {
+            int i = 1;
+
+            while (i < A / i)
+            {
+                i++;
+            }
+
+
+            if (i > A / i)
+            {
+                return --i;
+            }
+            else
+            {
+                return i;
+            }
+
+        }
+    }
+
+}
---
+++ InterviewBit_Tasks/Stringoholics/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stringoholics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var strList = new string[] { "a", "ababa", "aba" };
+            
+            Console.WriteLine(s.Solve(strList.ToList()));
+        }
+    }
+    public class Solution
+    {
+        public int Solve(List<string> A)
+        {
+            var orderedA = A.OrderByDescending(x => x).ToList();
+            var maxLen = orderedA[0].Length;
+            var result = 0;
+
+            var listOfResults = new List<List<int>>();
+
+
+            for(int i = 0; i<A.Count; i++)
+            {
+                var tmpStr = orderedA[i] + orderedA[i] + orderedA[i];
+                var strLen = orderedA[i].Length;
+                var tmpList = new List<int>(); 
+                
+                if(i != 0 && orderedA[i].Length == 1)
+                {
+                    tmpList = listOfResults[0];
+                }
+                else
+                {
+                    for (int j = 1; j <= strLen; j ++)
+                    {
+                        var tt = j % strLen;
+                        var jj = tt + j;
+                        var subStr = tmpStr.Substring(jj, strLen);
+                        if (orderedA[i] == subStr)
+                        {
+                            tmpList.Add(j);
+                        }
+                    }
+
+                }
+
+                listOfResults.Add(tmpList);
+            }
+
+            listOfResults.OrderByDescending(x => x.Count);
+
+            for(int i = 0; i < listOfResults.Count; i++)
+            {
+                listOfResults[i].OrderBy(x => x);
+            }
+
+            
+            for(int i = 0; i< listOfResults[0].Count; i++)
+            {
+
+            }
+            return result;
+            
+        }
+    }
+}
