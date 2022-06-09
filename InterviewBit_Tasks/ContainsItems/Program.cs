2dcc54d7919cd192fe7acf40e2a19dfe741ed810
Anastasia Florenko <aflorenko@leobit.com>
2022-06-09 19:14:53 +0300

    Add two exercises with arrays

--- InterviewBit_Tasks/ContainsItems/Program.cs
+++ InterviewBit_Tasks/ContainsItems/Program.cs
@@ -3,29 +3,64 @@
     static void Main(string[] args)
     {
         var A = new List<int>() { 4, 2, 1, 4 };
-        var B = new List<int>() { 7 , 8};
+        var B = new List<int>() { 7 , 8, 1};
 
         Console.WriteLine(Solution.ContainsItems(A, B));
-
+        Console.WriteLine(Solution.ContainsItemsWithLinq(A, B));
     }
 
     public class Solution
     {
+        //O(a + b) -Time 
+        //O(a) - space
         public static bool ContainsItems(List<int> A, List<int> B)
         {
-            var set = new HashSet<int>();
-            foreach (var item in A)
+            if(A.Count == 0 || B.Count == 0)
             {
-                set.Add(item);
+                return false;
             }
-
-            foreach (var item in B)
+            if (A.Count == 1)
             {
-                if (set.Contains(item))
+                return ContainsItem(B, A.First());
+            }
+            else if (B.Count == 1)
+            {
+                return ContainsItem(A, B.First());
+            }
+            else
+            {
+                var set = new HashSet<int>();
+                foreach (var item in A)
+                {
+                    set.Add(item);
+                }
+
+                foreach (var item in B)
+                {
+                    if (set.Contains(item))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool ContainsItemsWithLinq(List<int> A, List<int> B)
+        {
+            return A.Any(a => B.Contains(a));
+        }
+
+        private static bool ContainsItem(List<int> array, int item)
+        {
+            foreach(var a in array)
+            {
+                if(a == item)
                 {
                     return true;
                 }
             }
+
             return false;
         }
     }
---
+++ InterviewBit_Tasks/MergeSortedArrays/Program.cs
@@ -0,0 +1,61 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var list1 = new List<int>() { 1, 2, 4 , 10, 15};
+        var list2 = new List<int>() { 3, 6 , 8};
+
+        var mergedArray = Solution.MergeSortedArrays(list1, list2);
+
+        foreach (var m in mergedArray)
+        {
+            Console.Write($"{m} ");
+        }
+    }
+
+    public class Solution
+    {
+        public static List<int> MergeSortedArrays(List<int> list1, List<int> list2)
+        {
+            var result = new List<int>();
+
+            var len1 = list1.Count - 1;
+            var len2 = list2.Count - 1;
+            var index1 = 0;
+            var index2 = 0;
+
+            while (index1 <= len1 && index2 <= len2)
+            {
+                if(list1[index1] < list2[index2])
+                {
+                    result.Add(list1[index1]);
+                    index1++;
+                }
+                else
+                {
+                    result.Add(list2[index2]);
+                    index2++;
+                }
+            }
+
+            if(index1 <= len1)
+            {
+                for(int i = index1; i <= len1; i++)
+                {
+                    result.Add(list1[i]);
+                }
+            }
+            else if (index2 <= len2)
+            {
+                for (int i = index2; i <= len2; i++)
+                {
+                    result.Add(list2[i]);
+                }
+            }
+
+            return result;
+
+        }
+
+    }
+}
